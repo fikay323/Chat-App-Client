6db055cbf84908855231fc479fdf1e7a229c873f
Fagbenro Oluwafikayomi Caleb <fagbenrocoa@gmail.com>
2025-05-24 20:40:40 +0100

    Update base API URL to production server

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: "AIzaSyD1izR8l8ZWQGnaRBPTWmgBV0qbadLnzQo",
     authDomain: "chatapppush-fikky.firebaseapp.com",
@@ -8,6 +8,6 @@ export const environment = {
     appId: "1:165836175773:web:72af1b0ed099ced483d774"
   },
   vapidKey: "BDrZfRFUmdiRUjHM2g5i1d-jJSKgI_RjsUasJxJCfUmYp3dLz1vb01BWyd5YqXu_vQjerqc4VC59aMubregGZwQ",
-  // private readonly baseApiUrl = "https://chat-app-server-o90s.onrender.com/chatHub";
-  baseApiUrl: "http://localhost:5000/"
+  baseApiUrl: "https://chat-app-server-o90s.onrender.com/chatHub"
+//   baseApiUrl: "http://localhost:5000/"
 };
